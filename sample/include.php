8d9edad11c03721947612f3abcddf1ecc7ca589b
JacobNeergaard <neergaard@kananga.dk>
2026-09-21 12:28:46 +0200

    Update HealDocument ref

--- sample/include.php
+++ sample/include.php
@@ -4,7 +4,7 @@ WildFile is licensed under the Apache License 2.0 license
 https://github.com/trp-solutions/WildFile/blob/main/LICENSE
 */
 declare(strict_types=1);
-require_once('../../heal-document/lib/HealDocument.php'); // https://github.com/TRP-Solutions/heal-document
+require_once('../../HealDocument/lib/HealDocument.php'); // https://github.com/trp-solutions/HealDocument
 require_once('../lib/WildFile.php');
 
 define('STORAGE',__DIR__.'/../storage');
